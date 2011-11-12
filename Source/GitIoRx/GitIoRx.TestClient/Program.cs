ed971e55ad856572cc03003cb7032b57760a4784
Dima Pasko <dimapasko@gmail.com>
2011-11-12 23:56:31 +0200

    Project renamed

--- Source/GitIoRx/GitIoRx.TestClient/Program.cs
+++ Source/GitIoRx/GitIoRx.TestClient/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace GitIoRx.TestClient
+﻿namespace GitIoSharp.TestClient
 {
 	class Program
 	{
---
+++ Source/GitIoRx/GitIoRx/GitIoSharpClient.cs
@@ -0,0 +1,7 @@
+﻿namespace GitIoSharp
+{
+	public class GitIoSharpClient
+	{
+		 
+	}
+}
