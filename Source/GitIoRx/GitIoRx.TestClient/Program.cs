0fdad1358a54cbf001c606bac0c0e0acae1f1068
Dima Pasko <dimapasko@gmail.com>
2011-11-17 00:41:40 +0200

    First working version

--- Source/GitIoRx/GitIoRx.TestClient/Program.cs
+++ Source/GitIoRx/GitIoRx.TestClient/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace GitIoSharp.TestClient
+﻿using System;
+
+namespace GitIoSharp.TestClient
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			var r = new GitIo().Shorten(new Uri("http://github.com/dimapasko"));
 		}
 	}
 }
---
+++ Source/GitIoRx/GitIoRx/GitIo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace GitIoSharp
+{
+	public class GitIo
+	{
+		public Uri Shorten(Uri url)
+		{
+			var req = WebRequest.Create("http://git.io");
+			req.ContentType = "application/x-www-form-urlencoded";
+			req.Method = "POST";
+			//We need to count how many bytes we're sending. Post'ed Faked Forms should be name=value&
+			var bytes = System.Text.Encoding.ASCII.GetBytes("url=" + url);
+			req.ContentLength = bytes.Length;
+			Stream os = req.GetRequestStream();
+			os.Write(bytes, 0, bytes.Length);
+			os.Close();
+			var resp = req.GetResponse();
+			return new Uri(resp.Headers[HttpResponseHeader.Location]);
+		}
+
+		public void Shorten(string url, string code)
+		{
+		}
+	}
+}
